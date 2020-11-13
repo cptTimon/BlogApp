c60d27fda894498f2de6cc8df4c63242c888c5bc
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2020-11-13 20:22:51 +0100

    Task 6.3.3

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
   }); */
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -13,7 +14,7 @@
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
 }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
     /* [DONE] remove class 'active' from all articles */
@@ -22,11 +23,17 @@
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
-    /* get 'href' attribute from the clicked link */
-  
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const path = clickedElement.getAttribute('href');
+    console.log(path);
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+
+    const chosenArticle = document.querySelector(path);
+    console.log(chosenArticle);
     /* add class 'active' to the correct article */
+    chosenArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
